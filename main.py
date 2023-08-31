dc5ed846b5a8c479284107ed02303700a6fab4f6
ljp2 <ljp2@lehigh.edu>
2023-08-30 21:08:37 -0400

    working

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import pandas as pd
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
-import myapi
+
 
 current_file_path = os.path.abspath(__file__)
 parent_directory = os.path.dirname(current_file_path)
@@ -27,16 +27,13 @@ def plotCandles(plt:pg.PlotItem, df:pd.DataFrame):
         plt.addItem(pg.BarGraphItem(x=index, y0=bot, y1=top, width=40, brush=color))
 
 
-# today = datetime.now()
-# df = myapi.get_minute_bars_day_open_hours('SPY', today)
-# df.to_csv('bars.csv')
 
 df = pd.read_csv('Bars/Aug-26.csv',header=0, index_col='timestamp')
 
 app = pg.mkQApp("DateAxisItem Example")
 
 w = pg.PlotWidget(axisItems = {'bottom': pg.DateAxisItem()})
-w.resize(1200, 800)
+
 
 plotCandles(w, df)
 
